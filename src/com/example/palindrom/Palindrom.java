f23845c6d7edfab27711ef15d388a5464f091496
1Max58 <>
2023-11-08 18:51:11 +0200

    Input a word and converting its letters to lower case implemented

--- src/com/example/palindrom/Palindrom.java
+++ src/com/example/palindrom/Palindrom.java
@@ -1,7 +1,14 @@
 package com.example.palindrom;
 
+import java.util.Scanner;
+
 public class Palindrom {
     public static void main(String[] args) {
-
+        System.out.println("This program checks if typed word is a palindrom.");
+        System.out.print("Please enter a word: ");
+        Scanner input = new Scanner(System.in);
+        String word = input.next();
+        String wordLowerCase = word.toLowerCase();
+        System.out.println("Word after letters changed to lowercase: " + wordLowerCase);
     }
 }
